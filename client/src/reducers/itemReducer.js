4780807011bd5899543dee3737053e1a742c9bec
JLz403 <jluzz403@gmail.com>
2020-03-22 10:33:24 -0600

    create addItem modal

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -6,15 +6,25 @@ const initialState = {
     { id: uuidv4(), name: 'Eggs' },
     { id: uuidv4(), name: 'Milk' },
     { id: uuidv4(), name: 'Steak' },
-    { id: uuidv4(), name: 'Water' },
-  ],
+    { id: uuidv4(), name: 'Water' }
+  ]
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case GET_ITEMS:
       return {
+        ...state
+      }
+    case DELETE_ITEM:
+      return {
         ...state,
+        items: state.items.filter(items => items.id !== action.payload)
+      }
+    case ADD_ITEM:
+      return {
+        ...state,
+        items: [action.payload, ...state.items]
       }
     default:
       return state
