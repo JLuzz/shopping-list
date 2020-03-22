a61d75ea27ae04dabe3bf444b8b4141064ee84c6
JLz403 <jluzz403@gmail.com>
2020-03-22 16:04:42 -0600

    hook up backend

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import AppNavBar from './components/AppNavbar'
 import ShoppingList from './components/ShoppingList'
 import ItemModal from './components/ItemModal'
-import Containter, { Container } from 'reactstrap'
+import { Container } from 'reactstrap'
 
 import { Provider } from 'react-redux'
 import store from './store'
