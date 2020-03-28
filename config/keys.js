05e3417f1bfbd85243bb17371985202763ed4f1c
JLz403 <jluzz403@gmail.com>
2020-03-28 16:06:54 -0600

    fixing typo

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 const mongoURI =
-  'mongodb+srv://Jeremy:<TV0ZRYh9BRHC8kLA>@shopping-list-q5mjp.mongodb.net/test?retryWrites=true&w=majority'
+  'mongodb+srv://Jeremy:TV0ZRYh9BRHC8kLA@shopping-list-q5mjp.mongodb.net/test?retryWrites=true&w=majority'
 export default mongoURI
