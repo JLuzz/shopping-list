f4735e3390fc854301392fa49b474b9e22855786
JLz403 <jluzz403@gmail.com>
2020-04-01 00:29:17 -0600

    return statement addes to if branch

--- middlware/auth.js
+++ middlware/auth.js
@@ -5,7 +5,8 @@ export default (req, res, next) => {
   const token = req.header('x-auth-token')
 
   // Check for token
-  if (!token) res.status(401).json({ msg: 'No token, authorization denied' })
+  if (!token)
+    return res.status(401).json({ msg: 'No token, authorization denied' })
 
   try {
     // Verify token
