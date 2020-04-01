82cbdd57f85ade5dfdc5f3677f683028ec4555a5
JLz403 <jluzz403@gmail.com>
2020-03-31 23:03:07 -0600

    created auth middle ware

---
+++ middlware/auth.js
@@ -0,0 +1,19 @@
+import config from 'config'
+import jwt from 'jsonwebtoken'
+
+export default (req, res, next) => {
+  const token = req.header('x-auth-token')
+
+  // Check for token
+  if (!token) res.status(401).json({ msg: 'No token, authorization denied' })
+
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, config.get('jwtKey'))
+    // Add user from payload
+    req.user = decoded
+    next()
+  } catch (e) {
+    res.status(400).json({ msg: 'Token is not valid' })
+  }
+}
