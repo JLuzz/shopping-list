cc3fb2c0f78fb067a2525b9c84cdc2c898f52265
JLz403 <jluzz403@gmail.com>
2020-04-01 00:30:53 -0600

    create auth actions

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,47 @@
+import axios from 'axios'
+import { returnErrors } from './errorActions'
+
+import {
+  USER_LOADED,
+  USER_LOADING,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT_SUCCESS,
+  REGISTER_SUCCESS,
+  REGISTER_FAIL
+} from './types'
+
+// Check token & load user
+export const loadUser = () => (dispatch, getState) => {
+  //User Loading
+  dispatch({ type: USER_LOADING })
+
+  axios
+    .get('/api/auth/user', tokenConfig(getState))
+    .then(res => dispatch({ type: USER_LOADED, payload: res.data }))
+    .cathc(err => {
+      dispatch(returnErrors(err.response.data, err.response.status))
+      dispatch({ type: AUTH_ERROR })
+    })
+}
+
+//  setup config/headers and token
+export const tokenConfig = getState => {
+  // Get token from localstorage
+  const token = getState().auth.token
+
+  // Headers
+  const config = {
+    headers: {
+      'Content-type': 'application/json'
+    }
+  }
+
+  // IF token, add to headers
+  if (token) {
+    config.headers['x-auth-token'] = token
+  }
+
+  return config
+}
