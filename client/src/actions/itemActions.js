abb520c92d7825a847a4606f938f5e12248e29e3
JLz403 <jluzz403@gmail.com>
2020-04-26 21:29:17 -0600

    set up token config on item actions

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { tokenConfig } from './authActions'
 import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from './types'
 
 export const getItems = () => (dispatch) => {
@@ -11,8 +12,8 @@ export const getItems = () => (dispatch) => {
   )
 }
 
-export const addItem = (item) => (dispatch) => {
-  axios.post('/api/items', item).then((res) =>
+export const addItem = (item) => (dispatch, getState) => {
+  axios.post('/api/items', item, tokenConfig(getState)).then((res) =>
     dispatch({
       type: ADD_ITEM,
       payload: res.data,
@@ -20,8 +21,8 @@ export const addItem = (item) => (dispatch) => {
   )
 }
 
-export const deleteItem = (id) => (dispatch) => {
-  axios.delete(`/api/items/${id}`).then((res) =>
+export const deleteItem = (id) => (dispatch, getState) => {
+  axios.delete(`/api/items/${id}`, tokenConfig(getState)).then((res) =>
     dispatch({
       type: DELETE_ITEM,
       payload: id,
