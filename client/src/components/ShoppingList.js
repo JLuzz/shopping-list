86e3cb9d3b1efe382c2dc74d8bd8102c6b90bd19
JLz403 <jluzz403@gmail.com>
2020-05-04 21:08:39 -0600

    :recycle: move item modal to main container

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,11 +1,10 @@
 import React, { useEffect } from 'react'
-import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap'
+import { ListGroup, ListGroupItem, Button } from 'reactstrap'
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import { useSelector, useDispatch } from 'react-redux'
 import PropTypes from 'prop-types'
 
 import { getItems, deleteItem } from '../actions/itemActions'
-import ItemModal from '../components/ItemModal'
 
 const ShoppingList = () => {
   const dispatch = useDispatch()
@@ -18,34 +17,31 @@ const ShoppingList = () => {
   }, [dispatch])
 
   return (
-    <Container>
-      <ItemModal />
-      <ListGroup>
-        <TransitionGroup className="shopping-list">
-          {!itemsLoading &&
-            items.map(({ _id, name }) => (
-              <CSSTransition key={_id} timeout={500} classNames="fade">
-                <ListGroupItem>
-                  <Button
-                    className="remove-btn"
-                    color="danger"
-                    size="sm"
-                    onClick={() => dispatch(deleteItem(_id))}
-                  >
-                    &times;
-                  </Button>
-                  {name}
-                </ListGroupItem>
-              </CSSTransition>
-            ))}
-        </TransitionGroup>
-      </ListGroup>
-    </Container>
+    <ListGroup>
+      <TransitionGroup className="shopping-list">
+        {!itemsLoading &&
+          items.map(({ _id, name }) => (
+            <CSSTransition key={_id} timeout={500} classNames="fade">
+              <ListGroupItem>
+                <Button
+                  className="remove-btn"
+                  color="danger"
+                  size="sm"
+                  onClick={() => dispatch(deleteItem(_id))}
+                >
+                  &times;
+                </Button>
+                {name}
+              </ListGroupItem>
+            </CSSTransition>
+          ))}
+      </TransitionGroup>
+    </ListGroup>
   )
 }
 
-// ShoppingList.propTypes = {
-//   items: PropTypes.object.isRequired,
-// }
+ShoppingList.propTypes = {
+  items: PropTypes.object.isRequired,
+}
 
 export default ShoppingList
